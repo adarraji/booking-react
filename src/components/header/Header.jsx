08c5b97427aa70273509e55e1a66af85684356c9
adarraji <a.darraji@outlook.com>
2023-03-21 17:38:32 -0400

    added header list items

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,32 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
 import "./header.css"
+
+
 
 const Header = () => {
     return (
         <div className="header">
             <div className="headerList">
                 <div className="headerListItem">
-
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Stays</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlane} />
+                    <span></span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faCar} />
+                    <span>Cae rentals</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Attractions</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faTaxi} />
+                    <span>Airport taxis</span>
                 </div>
             </div>
         </div>
