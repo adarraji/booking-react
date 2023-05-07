cb75a04d57b640584a8d59a8ea0e6471b8b49883
adarraji <a.darraji@outlook.com>
2023-05-07 14:14:23 -0400

    added auth context

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,11 +8,14 @@ import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { useNavigate } from "react-router-dom";
 import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
 
 
 
 const Header = ({ type }) => {
     const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
+
     const [openDate, setOpenDate] = useState(false)
     const [dates, setDate] = useState([
         {
@@ -78,7 +81,7 @@ const Header = ({ type }) => {
                             Get rewarded for your travels – unlock instant savings of 10% or
                             more with a free Lamabooking account
                         </p>
-                        <button className="headerBtn">Sign in / Register</button>
+                        {!user && <button className="headerBtn">Sign in / Register</button>}
                         <div className="headerSearch">
                             <div className="headerSearchItem">
                                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
